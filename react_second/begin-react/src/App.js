cb3ebe69c1d45ab79b7d8dddf32f999769cde997
Sooji Kwon <gkdrhd6788@gmail.com>
2024-01-15 00:47:27 +0900

    learing through notion

--- react_second/begin-react/src/App.js
+++ react_second/begin-react/src/App.js
@@ -1,11 +1,73 @@
-import React from 'react'
-import Hello from './Hello'
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
+const initialState = {
+  inputs: {
+    username: '',
+    email: ''
+  },
+  users: [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
   return (
     <>
-      <Hello name="react" color="red" />
+      <CreateUser username={username} email={email} onChange={onChange}/>
+      <UserList users={users}/>
+      <div>활성사용자 수 : 0</div>
     </>
   );
 }
